836d799108e95bce290a03080c5e727c2e4ae3e5
walkr <walkr@local>
2015-05-15 15:44:17 -0700

    Edit example program

--- example/programd.py
+++ example/programd.py
@@ -3,8 +3,15 @@ import oi
 
 def main():
     program = oi.Program('my program', 'ipc:///tmp/programd.sock')
-    program.add_command('ping', lambda p: 'pong')
-    program.add_command('state', lambda p: p.state)
+
+    program.add_command(
+        'ping', lambda: 'pong')
+    program.add_command(
+        'state', lambda: program.state, 'show program state')
+    program.add_command(
+        'touch', lambda: setattr(program.state, 'touch', True) and True,
+        'touch state')
+
     program.run()
 
 if __name__ == '__main__':
