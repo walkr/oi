dec7214d65a900ace53d173a4abc9bb884a13eb8
walkr <walkr@local>
2015-05-14 21:29:59 -0700

    Improve testing

--- test/test_oi.py
+++ test/test_oi.py
@@ -21,5 +21,13 @@ class TestOi(unittest.TestCase):
     def test_new_ctl(self):
         self.assertIsNotNone(self.ctl)
 
-    def test_add_command(self):
+    def test_add_command_for_program(self):
         self.p.add_command('test', lambda p: 'test')
+
+    def test_add_local_command_for_ctl(self):
+        self.ctl.add_command('test', lambda p: 'test')
+        dest, res, err = self.ctl.call('test')
+
+        self.assertEqual(dest, 'local')
+        self.assertEqual(res, 'test')
+        self.assertIsNone(err)
