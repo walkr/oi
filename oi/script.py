226e8e487ba979ab3af6e09f9ac17cb5e34a4df6
walkr <walkr@local>
2015-05-17 23:33:55 -0700

    Update init script

--- oi/script.py
+++ oi/script.py
@@ -105,8 +105,8 @@ import oi
 
 def main():
     program = oi.Program('my program', 'ipc:///tmp/oi-random_string.sock')
-    program.add_command('ping', lambda p: 'pong')
-    program.add_command('state', lambda p: p.state)
+    program.add_command('ping', lambda: 'pong')
+    program.add_command('state', lambda: program.state)
     program.run()
 
 if __name__ == '__main__':
